b5f03996d5d70a6c5c9e520293f99e72d9f80ff9
yoyokity <1125404308@qq.com>
2024-10-03 20:37:33 +0800

    添加：防止程序重复打开

--- backendSrc/main.js
+++ backendSrc/main.js
@@ -22,6 +22,19 @@ Helper.init(process.cwd(), join(__dirname, '../'))
 await Scraper.load()
 
 //electron加载
+const gotTheLock = app.requestSingleInstanceLock() //防止重复启动
+if (!gotTheLock) {
+    app.quit()
+} else {
+    app.on('second-instance', (event, commandLine, workingDirectory) => {
+        // 如果用户再次启动应用，显示已打开的窗口
+        if (yoyoNode.window.mainWindow) {
+            if (yoyoNode.window.mainWindow.isMinimized()) yoyoNode.window.mainWindow.restore()
+            yoyoNode.window.mainWindow.focus()
+        }
+    })
+}
+
 app.enableSandbox()
 app.whenReady().then(() => {
     createWindow()
