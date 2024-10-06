257c144382315ba80991a9fe31fd10d257d12f3e
yoyokity <1125404308@qq.com>
2024-10-06 09:04:41 +0800

    update debug

--- backendSrc/debug/sessionDebug.js
+++ backendSrc/debug/sessionDebug.js
@@ -1,3 +1,11 @@
+////////////////////
+//修改参数
+let file_path = 'D:\\W2xExTmp\\新建文件夹\\性欲つよつよ 第1巻.mkv'
+let jav_num = '1283624'
+let scraper_name = 'getchu'
+let outputPath = 'D:\\W2xExTmp\\新建文件夹'
+/////////////////////////////
+
 //代理和翻译
 import Session from '../scraper/tool/session.js'
 import translate from '../scraper/tool/translator.js'
@@ -13,14 +21,6 @@ import { Helper } from '../../yo-electron-lib/Helper/helper.js'
 import { fileURLToPath } from 'url'
 import { join, dirname } from 'path'
 import Video from '../scraper/Video.js'
-
-////////////////////
-//修改参数
-let file_path = 'D:\\W2xExTmp\\新建文件夹\\性欲つよつよ 第1巻.mkv'
-let jav_num = '1283624'
-let scraper_name = 'getchu'
-let outputPath = 'D:\\W2xExTmp\\新建文件夹'
-/////////////////////////////
 
 //运行
 const __dirname = dirname(fileURLToPath(import.meta.url))
